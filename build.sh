8b9bb85b229946ece908dbf9ad1e2b30543742e6
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-07-27 13:47:40 +0300

    don't be that verbose when building

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 GIT_TAG=$(git describe --tags --abbrev=0)
 BUILD_MACHINE=$(uname -s -n)
 BUILD_DATE=$(date)
-go build -x -ldflags "-X \"$CONFIG_MOD.GitCommit=$GIT_COMMIT\" \
-                      -X \"$CONFIG_MOD.GitTag=$GIT_TAG\" \
-                      -X \"$CONFIG_MOD.BuildMachine=$BUILD_MACHINE\" \
-                      -X \"$CONFIG_MOD.BuildDate=$BUILD_DATE\""
+go build -ldflags "-X \"$CONFIG_MOD.GitCommit=$GIT_COMMIT\" \
+                   -X \"$CONFIG_MOD.GitTag=$GIT_TAG\" \
+                   -X \"$CONFIG_MOD.BuildMachine=$BUILD_MACHINE\" \
+                   -X \"$CONFIG_MOD.BuildDate=$BUILD_DATE\""
